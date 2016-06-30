3ffe0fd32eff976abfcd4fd289a67c08c64e174c
José Cerón <Jose@Jose-Macbook.local>
2016-06-30 10:33:08 +0200

    Fixes #3

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -8,7 +8,7 @@ case class Owner(ownerName: String, id: Long)
 object Owner {
   implicit val ownerReads: Reads[Owner] = (
     (JsPath \ "login").read[String] and
-    (JsPath \ "id").read[Long])(Owner.apply _)
+      (JsPath \ "id").read[Long]) (Owner.apply _)
 }
 
 case class Repository(repoName: String, owner: Owner)
@@ -16,7 +16,7 @@ case class Repository(repoName: String, owner: Owner)
 object Repository {
   implicit val repositoryReads: Reads[Repository] = (
     (JsPath \ "name").read[String] and
-    (JsPath \ "owner").read[Owner])(Repository.apply _)
+      (JsPath \ "owner").read[Owner]) (Repository.apply _)
 }
 
 case class Milestone(id: Long, milestoneName: String)
@@ -24,16 +24,16 @@ case class Milestone(id: Long, milestoneName: String)
 object Milestone {
   implicit val milestoneReads: Reads[Milestone] = (
     (JsPath \ "id").read[Long] and
-    (JsPath \ "title").read[String])(Milestone.apply _)
+      (JsPath \ "title").read[String]) (Milestone.apply _)
 }
 
-case class Issue(id: Long, issueName: String, milestone: Option[Milestone])
+case class Issue(id: Long, issueName: String, milestone: Milestone)
 
 object Issue {
   implicit val issueReads: Reads[Issue] = (
     (JsPath \ "id").read[Long] and
-    (JsPath \ "title").read[String] and
-    (JsPath \ "milestone").readNullable[Milestone])(Issue.apply _)
+      (JsPath \ "title").read[String] and
+      (JsPath \ "milestone").read[Milestone].orElse(Reads.pure(new Milestone(0, "(No milestone)")))) (Issue.apply _)
 }
 
 case class MainModel(repository: Repository, issues: Seq[Issue])
